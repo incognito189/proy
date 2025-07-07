91a437c06590e670d70698f0b2e7fccfa2c2c4fa
incognito189 <ganonymous407@gmail.com>
2025-07-07 16:33:26 -0400

    Update urls.py

--- proyect-main/mi_proyecto/mi_proyecto/urls.py
+++ proyect-main/mi_proyecto/mi_proyecto/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from mi_app import views  # Asegúrate que esta importación es correcta
+from django.urls import path, include
+from django.views.generic.base import RedirectView
+from mi_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'),  # Ahora la vista existe
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home, name='home'),
+    path('', RedirectView.as_view(url='/login/', permanent=True)),  # Redirige raíz a login
 ]
